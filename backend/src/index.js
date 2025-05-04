573e25c26004541145274e2dd6b865ccfb1cd009
AitezazAhmed <stoneghold4103@gmail.com>
2025-05-04 20:50:56 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,30 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import cors from "cors"
-import path from "path";
-import { connectDB } from "./lib/db.js"; // your custom DB connection
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import{app,server} from "../src/lib/socket.js"
-app.use(express.json({ limit: '10mb' })); // or even higher like '20mb'
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
-// Connect to MongoDB
-connectDB();
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
@@ -31,10 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Fallback port
-const PORT = process.env.PORT;
-
 server.listen(PORT, () => {
-  console.log("Server is running on port: " +PORT);
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
-
