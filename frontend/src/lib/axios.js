5689831a4058d53714fa49d19f8e1a9103817fe2
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-13 03:34:23 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "https://talk-shak-7.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
 });
   
