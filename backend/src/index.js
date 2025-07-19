20ec09adb715036a637ec6b802654f2bdd332ff6
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-19 14:27:37 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,13 +26,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
