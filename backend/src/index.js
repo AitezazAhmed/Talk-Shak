9976c8fd28bf6f98684ee759b553d5a834fd36cd
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-19 14:19:15 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://talk-shak-p5bw.vercel.app/login"],
+    origin: ["http://localhost:5173","https://talk-shak-p5bw.vercel.app"],
     credentials: true
   })
 );
