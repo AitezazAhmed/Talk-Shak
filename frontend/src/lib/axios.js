605a626aead1633fa1f387850dc9826e0e871dd7
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-19 14:29:09 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  withCredentials: true, // allows cookies (for JWT)
 });
-  
