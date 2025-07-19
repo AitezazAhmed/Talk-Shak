27bcf6ca9ee191bc8849651bf8e96a3e8921e34a
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-19 15:14:45 +0500

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -84,7 +84,7 @@ export const useAuthStore=create((set,get)=>({
       connectSocket:()=>{
         const {authUser}=get()
         if(!authUser || get().socket?.connected) return;
-       const socket = io(https://talk-shak-8.onrender.com/api, {
+       const socket = io("https://talk-shak-8.onrender.com", {
   query: { userId: authUser._id },
   withCredentials: true,
   transports: ["polling"],
